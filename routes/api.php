3c457cdc8c1789d93dcf4d505c36d76b5ec37277
Adam Children <adam.children@themix.org.uk>
2024-06-21 00:18:55 +0100

    feat: init pseudo api endpoint routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(static function () {
+    /**
+     * TODO: Out of scope of assignment
+     *  - Get [Index]
+     *  - Patch
+     *  - Delete
+     */
+    Route::prefix('/api/user/{user}/isa')->group(static function () {
+        Route::get('/');
+        Route::post('/');
+    });
+});
