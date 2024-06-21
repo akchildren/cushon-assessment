8705889336ee799a88d13ea627fab2f541cf6f29
Adam Children <adam.children@themix.org.uk>
2024-06-22 00:17:29 +0100

    tests: init InvestmentController

---
+++ app/Http/Controllers/InvestmentController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+final class InvestmentController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // TODO: Task Requirement
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // TODO: Task Requirement
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Todo: Outside assignment scope
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // Todo: Outside assignment scope
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // Todo: Outside assignment scope
+    }
+}
