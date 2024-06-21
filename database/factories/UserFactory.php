e6b1657bf7d0c8409a237307559172e133400891
Adam Children <adam.children@themix.org.uk>
2024-06-22 00:16:32 +0100

    tests: add employee state option to user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,12 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
@@ -40,5 +42,17 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    /**
+     * Indicate that the user is an employee
+     */
+    public function employee(?Company $company = null): static
+    {
+        $company ??= Company::factory()->create();
+
+        return $this->afterCreating(
+            fn (User $user) => $user->company()->associate($company)
+        );
     }
 }
