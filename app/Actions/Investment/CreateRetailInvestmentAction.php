b525691eb584954ebd8939e9a780bf0ac9e1831a
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:35:19 +0100

    feat: init CreateRetailInvestmentAction

---
+++ app/Actions/Investment/CreateRetailInvestmentAction.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Actions\Investment;
+
+use App\Actions\Action;
+
+/**
+ * Basic action to create retail investment
+ */
+final readonly class CreateRetailInvestmentAction extends CreateInvestmentAction {}
