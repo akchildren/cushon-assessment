81c6ea28c4928629aadc703d50c66ed3d6d3589a
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:39:32 +0100

    feat: init get routes for investments

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Controllers\Investment\AccountInvestmentsIndexController;
 use App\Http\Controllers\Investment\CreateAccountInvestmentController;
+use App\Http\Controllers\Investment\GetInvestmentController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->group(static function () {
     /**
      * TODO: Out of scope of assignment
-     *  - Get [Index]
      *  - Patch
      *  - Delete
      */
     Route::prefix('account/{account}/investment')->group(static function () {
-        Route::get('/', static fn () => 'todo');
+        Route::get('/', AccountInvestmentsIndexController::class);
         Route::post('/', CreateAccountInvestmentController::class);
+    });
+
+    Route::prefix('investment/{investment')->group(static function () {
+        Route::get('/', GetInvestmentController::class);
     });
 });
