4556b46c72386eaf195fed23c6a8cda93629847e
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:33:01 +0100

    refactor: Rename FundInvestment + remove unused traits/fillables

--- app/Models/Investment/FundInvestment.php
+++ app/Models/Investment/FundInvestment.php
@@ -3,20 +3,15 @@
 namespace App\Models\Investment;
 
 use Cknow\Money\Casts\MoneyIntegerCast;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-final class InvestmentFund extends Model
+final class FundInvestment extends Pivot
 {
-    use HasFactory;
-    use HasUuids;
     use SoftDeletes;
 
     protected $fillable = [
         'amount',
-        'currency_iso',
     ];
 
     protected $casts = [
