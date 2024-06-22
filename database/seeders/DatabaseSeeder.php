5f212b876f134aac74c799b4c50ee4668065e077
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:30:42 +0100

    feat: DatabaseSeeder update calls

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\Fund;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+final class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
-
-        // Allowing seeding of 5 random funds for purpose of showcase
-        Fund::factory(5)->create();
+        $this->call(FundSeeder::class);
+        $this->call(AccountTypeSeeder::class);
     }
 }
