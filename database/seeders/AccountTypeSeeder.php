1eb8903722c7d35819db8deace563d27c1a05c9f
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:30:15 +0100

    feat: init AccountTypeSeeder

---
+++ database/seeders/AccountTypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enum\IsaType;
+use App\Models\Account\AccountType;
+use Illuminate\Database\Seeder;
+
+final class AccountTypeSeeder extends Seeder
+{
+    private array $accountTypes = [
+        IsaType::ISA,
+        IsaType::LIFETIME_ISA,
+        IsaType::JUNIOR_ISA,
+    ];
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->accountTypes as $accountType) {
+            AccountType::updateOrCreate([
+                'name' => $accountType->value,
+            ]);
+        }
+    }
+}
