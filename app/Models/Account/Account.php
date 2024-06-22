88f5e28f74a6b6dc152b024b8b24488e2a64c617
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:34:22 +0100

    feat: Account fillable + amount accessor

--- app/Models/Account/Account.php
+++ app/Models/Account/Account.php
@@ -4,6 +4,8 @@ namespace App\Models\Account;
 
 use App\Models\Investment\Investment;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,11 +14,28 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+/**
+ * @mixin Builder<Account>
+ */
 final class Account extends Model
 {
     use HasFactory;
     use HasUuids;
     use SoftDeletes;
+
+    protected $fillable = [
+        'account_type_id',
+    ];
+
+    /**
+     * @note: For MVP, I have excluded any logic surrounding interested accrued on ISA, this will just return the investments totals
+     */
+    public function amount(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->investments()?->getFundTotal()
+        );
+    }
 
     /**
      * -------------------------------------------------
