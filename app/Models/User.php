9c271c2fd7586692ec29e0040a5d42fad5c2e03b
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:32:26 +0100

    feat: user model to inherit HasApiToken sanctum trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,9 +9,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 final class User extends Authenticatable
 {
+    use HasApiTokens;
     use HasFactory;
     use HasUuids;
     use Notifiable;
