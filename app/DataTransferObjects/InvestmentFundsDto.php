a95216d8589360c840ce9e86a34995c51a3cba08
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:36:02 +0100

    feat: init InvestmentFundsDto

---
+++ app/DataTransferObjects/InvestmentFundsDto.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\DataTransferObjects;
+
+final class InvestmentFundsDto implements DataTransferObject
+{
+    public function __construct(
+        public array $data,
+    ) {}
+
+    public static function fromArray(array $data): static
+    {
+        return new self(
+            array_map(
+                static fn ($fundData) => InvestmentFundDto::fromArray($fundData),
+                $data
+            )
+        );
+    }
+
+    public function toArray(): array
+    {
+        return $this->data;
+    }
+}
