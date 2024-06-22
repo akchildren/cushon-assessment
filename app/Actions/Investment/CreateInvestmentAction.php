3332a76ccebc0ca0a54611ac8c86360978f7f306
Adam Children <adam.children@themix.org.uk>
2024-06-23 00:35:28 +0100

    feat: init CreateInvestmentAction

--- app/Actions/Investment/CreateInvestmentAction.php
+++ app/Actions/Investment/CreateInvestmentAction.php
@@ -3,23 +3,29 @@
 namespace App\Actions\Investment;
 
 use App\Actions\Action;
+use App\DataTransferObjects\InvestmentFundsDto;
 use app\Models\Account\Account;
+use App\Models\Investment\Investment;
 
 /**
  * Basic action to create investment
  */
-final readonly class CreateInvestmentAction extends Action
+abstract readonly class CreateInvestmentAction extends Action
 {
     public function __construct() {}
 
     public function handle(
         Account $account,
-        array $funds,
-    ): Account {
-        // Todo : Logic to create investment and link funds
+        InvestmentFundsDto $investmentFundsDto,
+    ): Investment {
         $investment = $account->investments()->create();
-        $investment->funds()->sync($funds);
 
-        return $account->refresh();
+        foreach ($investmentFundsDto->toArray() as $fund) {
+            $investment
+                ->funds()
+                ->attach($fund->id, ['amount' => $fund->amount]);
+        }
+
+        return $investment->refresh();
     }
 }
