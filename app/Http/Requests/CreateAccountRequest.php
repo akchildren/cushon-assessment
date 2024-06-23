82d265ccc3c4a3aa70dddba7d84a304ad40e6c3d
Adam Children <adam.children@themix.org.uk>
2024-06-24 00:51:16 +0100

    feat: init CreateAccountRequest

---
+++ app/Http/Requests/CreateAccountRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\DataTransferObjects\AccountTypeDto;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+final class CreateAccountRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'account_type' => [
+                'required',
+                'exists:account_types,name',
+            ],
+        ];
+    }
+
+    public function getAccountTypeDto(): AccountTypeDto
+    {
+        return AccountTypeDto::fromArray($this->safe(['account_type']));
+    }
+}
