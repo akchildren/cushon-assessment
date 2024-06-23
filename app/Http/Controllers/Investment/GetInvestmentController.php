ea59d3bc2f9635d08b39e8f5443fbdb2c78a9872
Adam Children <adam.children@themix.org.uk>
2024-06-23 22:21:18 +0100

    feat: add policy gate to GetInvestmentController

--- app/Http/Controllers/Investment/GetInvestmentController.php
+++ app/Http/Controllers/Investment/GetInvestmentController.php
@@ -5,12 +5,15 @@ namespace App\Http\Controllers\Investment;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\InvestmentResource;
 use App\Models\Investment\Investment;
+use Illuminate\Support\Facades\Gate;
 
 final class GetInvestmentController extends Controller
 {
     public function __invoke(
         Investment $investment,
     ): InvestmentResource {
+        Gate::authorize('view', $investment);
+
         return new InvestmentResource($investment);
     }
 }
