5c1225e0fa5560fe22b0068fcb6a8fa3014173f2
Adam Children <adam.children@themix.org.uk>
2024-06-23 22:23:42 +0100

    tests: GetInvestmentTest

---
+++ tests/Feature/Isa/GetInvestmentTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Isa;
+
+use App\Models\Investment\Investment;
+use App\Models\User;
+use Cknow\Money\Money;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+final class GetInvestmentTest extends TestCase
+{
+    private const string ENDPOINT = '/api/investment/%s';
+
+    private User $user;
+
+    private Investment $investment;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+        $this->user = User::factory()->hasIsaAccount()->create();
+        $this->investment = Investment::factory()->hasFunds()->create([
+            'account_id' => $this->user->accounts()->first()->id,
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetCustomerInvestment(): void
+    {
+        Sanctum::actingAs($this->user);
+
+        $this->getJson($this->getEndpoint())
+            ->assertStatus(200)
+            ->assertJson(fn(AssertableJson $json) => $json
+                ->where('data.id', $this->investment->id)
+                ->where('data.account_id', $this->user->accounts()->first()->id)
+                ->where('data.amount', Money::parse($this->investment->amount)->formatByDecimal())
+            );
+    }
+
+    public function testOnlyCustomerOfInvestmentCanView(): void
+    {
+        $this->user = User::factory()->create();
+        Sanctum::actingAs($this->user);
+
+        $this->getJson($this->getEndpoint())
+            ->assertStatus(403);
+    }
+
+    private function getEndpoint(): string
+    {
+        return sprintf(
+            self::ENDPOINT,
+            $this->investment->id
+        );
+    }
+}
