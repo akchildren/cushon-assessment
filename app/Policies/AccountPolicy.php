0f94fb5c1162870dff47dce7d613f4afb0d6b723
Adam Children <adam.children@themix.org.uk>
2024-06-23 22:20:36 +0100

    feat: init AccountPolicy

---
+++ app/Policies/AccountPolicy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Account\Account;
+use App\Models\User;
+
+final readonly class AccountPolicy
+{
+    public function view (User $user, Account $account): bool
+    {
+        return $user->id === $account->user->id;
+    }
+}
