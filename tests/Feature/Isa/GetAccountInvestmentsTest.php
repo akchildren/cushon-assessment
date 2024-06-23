17a1a5889a17302cf0fea939dd78dfa05e5270af
Adam Children <adam.children@themix.org.uk>
2024-06-23 22:23:31 +0100

    tests: GetAccountInvestmentsTest

---
+++ tests/Feature/Isa/GetAccountInvestmentsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Isa;
+
+use App\Models\Account\Account;
+use App\Models\Investment\Investment;
+use App\Models\User;
+use Laravel\Sanctum\Sanctum;
+use Tests\TestCase;
+
+final class GetAccountInvestmentsTest extends TestCase
+{
+    private const string ENDPOINT = '/api/account/%s/investment';
+
+    private Account $account;
+
+    private User $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->seed();
+
+        $this->user = User::factory()->hasIsaAccount()->create();
+        $this->account = $this->user->accounts()->first();
+
+        Investment::factory(2)->hasFunds()->create([
+            'account_id' => $this->account->id,
+        ]);
+    }
+
+    /**
+     * @return void
+     */
+    public function testGetAccountInvestments(): void
+    {
+        Sanctum::actingAs($this->user);
+
+        $this->getJson($this->getEndpoint())
+            ->assertStatus(200)
+            ->assertJsonCount(2, 'data');
+    }
+
+    public function testOnlyOwnerOfAccountCanViewInvestments(): void
+    {
+        $this->user = User::factory()->create();
+        Sanctum::actingAs($this->user);
+
+        $this->getJson($this->getEndpoint())
+            ->assertStatus(403);
+    }
+
+    private function getEndpoint(): string
+    {
+        return sprintf(
+            self::ENDPOINT,
+            $this->account->id
+        );
+    }
+}
