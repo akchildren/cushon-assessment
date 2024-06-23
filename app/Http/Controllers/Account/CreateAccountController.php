5d041f86182179097bb3cf70dbcec346533d7687
Adam Children <adam.children@themix.org.uk>
2024-06-24 00:51:49 +0100

    feat: init CreateAccountController

---
+++ app/Http/Controllers/Account/CreateAccountController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Account;
+
+use App\Actions\Account\CreateEmployeeAccountAction;
+use App\Actions\Account\CreateRetailAccountAction;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateAccountRequest;
+use App\Http\Resources\AccountResource;
+use Illuminate\Support\Facades\Auth;
+
+final class CreateAccountController extends Controller
+{
+    public function __invoke(
+        CreateAccountRequest $request,
+        CreateEmployeeAccountAction $createEmployeeAccountAction,
+        CreateRetailAccountAction $createRetailAccountAction,
+    ): AccountResource {
+        $action = match (Auth::user()?->company()->exists()) {
+            true => $createEmployeeAccountAction,
+            false => $createRetailAccountAction,
+        };
+
+        $result = $action->handle(
+            $request->user(),
+            $request->getAccountTypeDto()->accountType
+        );
+
+        return new AccountResource($result);
+    }
+}
