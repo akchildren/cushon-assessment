0caeb13f35c487838da588757e519e82c52cdf5a
Adam Children <adam.children@themix.org.uk>
2024-06-23 22:21:47 +0100

    refactor: correct investment relation to account

--- app/Models/Investment/Investment.php
+++ app/Models/Investment/Investment.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -23,9 +24,9 @@ final class Investment extends Model
         );
     }
 
-    public function account(): HasOne
+    public function account(): BelongsTo
     {
-        return $this->hasOne(Account::class);
+        return $this->belongsTo(Account::class);
     }
 
     public function funds(): BelongsToMany
