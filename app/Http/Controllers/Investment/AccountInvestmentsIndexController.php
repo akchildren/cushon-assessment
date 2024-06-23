2d61b95645a12efa61df66b478f55867864cf710
Adam Children <adam.children@themix.org.uk>
2024-06-23 22:21:08 +0100

    feat: add policy gate to AccountInvestmentsIndexController

--- app/Http/Controllers/Investment/AccountInvestmentsIndexController.php
+++ app/Http/Controllers/Investment/AccountInvestmentsIndexController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\InvestmentResource;
 use App\Models\Account\Account;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Gate;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 final class AccountInvestmentsIndexController extends Controller
@@ -13,10 +14,12 @@ final class AccountInvestmentsIndexController extends Controller
     public function __invoke(
         Account $account,
     ): AnonymousResourceCollection {
+        Gate::authorize('view', $account);
+
         if (! $account->investments()->exists()) {
             throw new NotFoundHttpException();
         }
 
-        return InvestmentResource::collection($account->investments());
+        return InvestmentResource::collection($account->investments);
     }
 }
