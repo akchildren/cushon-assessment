64cd64549ce06ac40858f83522b92006ef89e60a
Adam Children <adam.children@themix.org.uk>
2024-06-23 22:22:09 +0100

    tests: move Account factories to relative subdir

--- database/factories/Account/AccountFactory.php
+++ database/factories/Account/AccountFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Account;
 
 use App\Enum\IsaType;
 use app\Models\Account\Account;
--- database/factories/Account/AccountTypeFactory.php
+++ database/factories/Account/AccountTypeFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Account;
 
 use app\Models\Account\Account;
 use Illuminate\Database\Eloquent\Factories\Factory;
