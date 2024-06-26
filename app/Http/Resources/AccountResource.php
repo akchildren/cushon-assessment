4078c18f4594cf7e09b586cc99a0eb67d60b70d5
Adam Children <adam.children@themix.org.uk>
2024-06-27 00:48:03 +0100

    refactor: remove comments from resources

--- app/Http/Resources/AccountResource.php
+++ app/Http/Resources/AccountResource.php
@@ -8,11 +8,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 final class AccountResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
-     * @todo : Add to docs about active accounts
-     * @todo : Add to docs about interest calculations etc
-     *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
@@ -22,7 +17,6 @@ final class AccountResource extends JsonResource
             'user_id' => (string) $this->user_id,
             'account_type' => (string) $this->accountType?->name,
             'investments' => InvestmentResource::collection($this->investments),
-            //            'amount' => $this->amount ? Money::parse($this->amount)->format() : null,
             'created_at' => (string) $this->created_at->toDateTimeString(),
         ];
     }
--- app/Http/Resources/FundInvestmentResource.php
+++ app/Http/Resources/FundInvestmentResource.php
@@ -9,8 +9,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 final class FundInvestmentResource extends JsonResource
 {
     /**
-     * Transform the resource into an array.
-     *
      * @return array<string, mixed>
      */
     public function toArray(Request $request): array
